11123e71c90d1fa750ad2a69db7030dbc2223d12
Foysal Ahamed <cfaion341@gmail.com>
2021-01-15 09:45:16 +0000

    Initial Commit

---
+++ macnpm/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  macnpm
+//
+//  Created by Foysal Ahamed on 15/01/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ macnpm/macnpmApp.swift
@@ -0,0 +1,17 @@
+//
+//  macnpmApp.swift
+//  macnpm
+//
+//  Created by Foysal Ahamed on 15/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct macnpmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
